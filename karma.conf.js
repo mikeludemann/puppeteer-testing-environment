fb11455c2f9b0709f5034ddc7c8b0c5d75ff22b7
Mike Ludemann <mike-ludemann@hotmail.de>
2019-11-22 15:12:11 +0100

    Update karma.conf.js and package.json

--- karma.conf.js
+++ karma.conf.js
@@ -54,7 +54,7 @@ module.exports = function(config) {
 
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		browsers: ['Chrome', 'ChromeHeadless'],
+		browsers: ['ChromeHeadless'],
 
 		customLaunchers: {
 			MyHeadlessChrome: {
@@ -65,7 +65,7 @@ module.exports = function(config) {
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
-		// singleRun: false,
+		//singleRun: true,
 
 		// Concurrency level
 		// how many browser should be started simultaneous
